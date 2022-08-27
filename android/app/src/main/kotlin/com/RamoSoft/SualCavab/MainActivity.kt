1676d3cdc49a02331cbb53c40b519117de50d0fb
ramo828 <illegalism666@gmail.com>
2022-08-27 18:31:33 +0400

    Clean

---
+++ android/app/src/main/kotlin/com/RamoSoft/SualCavab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.RamoSoft.SualCavab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/ses/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ses
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
